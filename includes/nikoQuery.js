553c4e5b0ec70256d2d6509869287e777b5f4445
Niko <niko@savas.ca>
2017-04-29 19:15:54 -0700

    nikoQuery now supports Edge

--- includes/nikoQuery.js
+++ includes/nikoQuery.js
@@ -1,117 +1,120 @@
-var n$ = function(selector) {
+/*
+	TODO better encapsulation of these methods
+*/
 
-	//TODO need a new name for selector that's more explicit
-	var findElement = function(selector, parentDOM) {
-		if (typeof selector == "string"){
-			// Type 1: string selector
-			var selectorChar = selector.charAt(0);
-			var selectorQuery = selector.substring(1);
-			switch (selectorChar) {
-				case "#":
-					return n$(document.getElementById(selectorQuery));
-				case ".":
-					elementsFound = parentDOM.getElementsByClassName(selectorQuery);
-					n$Elements = []
-					for (var i=0;i<elementsFound.length;i++) {
-						n$Elements.push(n$(elementsFound[i]));
-					}
-					return n$Elements;
-				default:
-					// Get element(s) by name
-					elementsFound = parentDOM.getElementsByTagName(selector);
-					n$Elements = []
-					for (var i=0;i<elementsFound.length;i++) {
-						n$Elements.push(n$(elementsFound[i]));
-					}
-					return n$Elements;
-			}
-		} else if (selector.constructor.name == "n$Element") {
-			return selector;
-		} else {
-			// Type 2: given a DOM node (for now, no error checking)
-			return new n$Element(selector);
+var n$Element = function(el) {
+	this.element = el;
+
+	this.show = function() {
+		el.style.display = "";
+		el.className = el.className.replace(/ ?hidden/, "");
+	}
+
+	this.hide = function() {
+		if(el.className.indexOf("hidden") == -1 ){
+			el.className += " hidden";
 		}
 	}
 
-	var n$Element = function(el) {
-		this.element = el;
+	this.onClick = function(callback) {
+		el.addEventListener("click", callback);
+	}
 
-		this.show = function() {
-			el.style.display = "";
-			el.className = el.className.replace(/ ?hidden/, "");
-		}
-
-		this.hide = function() {
-			if(el.className.indexOf("hidden") == -1 ){
-				el.className += " hidden";
+	this.onTap = function(callback) {
+		var touchmoved;
+		el.addEventListener("touchstart", function() {
+			touchmoved = false;
+		});
+		el.addEventListener("touchmove", function() {
+			touchmoved = true;
+		});
+		el.addEventListener("touchend", function(e){
+			if (!touchmoved) {
+				callback(e);
 			}
-		}
+		});
+	}
 
-		this.onClick = function(callback) {
-			el.addEventListener("click", callback);
-		}
+	this.clone = function() {
+		return n$(el.cloneNode(true));
+	}
 
-		this.onTap = function(callback) {
-			var touchmoved;
-			el.addEventListener("touchstart", function() {
-				touchmoved = false;
-			});
-			el.addEventListener("touchmove", function() {
-				touchmoved = true;
-			});
-			el.addEventListener("touchend", function(e){
-				if (!touchmoved) {
-					callback(e);
+	this.html = function(html) {
+		el.innerHTML = html;
+	}
+
+	this.append = function(toAppend) {
+		if (typeof toAppend == "string") {
+			el.innerHTML += toAppend;
+		} else {
+			//Assume DOM node
+			el.appendChild(toAppend.element);
+		}
+	}
+
+	this.remove = function() {
+		el.parentNode.removeChild(el);
+	}
+
+	this.attr = function(attrName, value) {
+		if (value !== undefined) {
+			el.setAttribute(attrName, value);
+		} else {
+			return el.getAttribute(attrName);
+		}
+	}
+
+	this.removeAttr = function(attrName) {
+		el.removeAttribute(attrName);
+	}
+
+	this.children = function(selector) {
+		return findElement(selector, el);
+	}
+
+	this.addClass = function(className) {
+		el.className += " " + className;
+	}
+
+	this.removeClass = function(className) {
+		el.className = el.className.replace(/ ?className/, "");
+	}
+};
+
+//TODO need a new name for selector that's more explicit
+var findElement = function(selector, parentDOM) {
+	if (typeof selector == "string"){
+		// Type 1: string selector
+		var selectorChar = selector.charAt(0);
+		var selectorQuery = selector.substring(1);
+		switch (selectorChar) {
+			case "#":
+				return n$(document.getElementById(selectorQuery));
+			case ".":
+				elementsFound = parentDOM.getElementsByClassName(selectorQuery);
+				n$Elements = []
+				for (var i=0;i<elementsFound.length;i++) {
+					n$Elements.push(n$(elementsFound[i]));
 				}
-			});
+				return n$Elements;
+			default:
+				// Get element(s) by name
+				elementsFound = parentDOM.getElementsByTagName(selector);
+				n$Elements = []
+				for (var i=0;i<elementsFound.length;i++) {
+					n$Elements.push(n$(elementsFound[i]));
+				}
+				return n$Elements;
 		}
+	} else if (selector instanceof n$Element) {
+		return selector;
+	} else {
+		// Type 2: given a DOM node (for now, no error checking)
+		return new n$Element(selector);
+	}
+};
 
-		this.clone = function() {
-			return n$(el.cloneNode(true));
-		}
-
-		this.html = function(html) {
-			el.innerHTML = html;
-		}
-
-		this.append = function(toAppend) {
-			if (typeof toAppend == "string") {
-				el.innerHTML += toAppend;
-			} else {
-				//Assume DOM node
-				el.appendChild(toAppend.element);
-			}
-		}
-
-		this.remove = function() {
-			el.parentNode.removeChild(el);
-		}
-
-		this.attr = function(attrName, value) {
-			if (value !== undefined) {
-				el.setAttribute(attrName, value);
-			} else {
-				return el.getAttribute(attrName);
-			}
-		}
-
-		this.removeAttr = function(attrName) {
-			el.removeAttribute(attrName);
-		}
-
-		this.children = function(selector) {
-			return findElement(selector, el);
-		}
-
-		this.addClass = function(className) {
-			el.className += " " + className;
-		}
-
-		this.removeClass = function(className) {
-			el.className = el.className.replace(/ ?className/, "");
-		}
-	};
-
+var n$ = function(selector) {
 	return findElement(selector, document);
 }
 
